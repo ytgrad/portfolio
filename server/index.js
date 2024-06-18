30c6908efc7c7495579b189c4b309d5ebab3e7f8
ytgrad <ytgrad17@gmail.com>
2024-06-18 09:45:37 +0530

    server

--- server/index.js
+++ server/index.js
@@ -10,6 +10,12 @@ app.get("/download/quiz", (req, res) => {
 app.get("/download/calculator", (req, res) => {
   res.download("./dist/calculator.apk");
 });
+app.get("/download/stopwatch", (req, res) => {
+  res.download("./dist/stopwatch.apk");
+});
+app.get("/download/countdown", (req, res) => {
+  res.download("./dist/countdown.apk");
+});
 
 app.listen(PORT, () => {
   console.log(`listening on ${PORT}`);
